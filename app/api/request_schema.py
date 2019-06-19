3e15e45f67f7b18733809e27aa3f084de9fb2b72
Kelvin <kelvindsmn@gmail.com>
2019-06-19 09:32:12 +0700

    update dockerfile to multistage build

--- app/api/request_schema.py
+++ app/api/request_schema.py
@@ -7,7 +7,7 @@ from werkzeug.datastructures import FileStorage
 class UserRequestSchema:
     """Define all mandatory argument for creating user"""
     parser = reqparse.RequestParser()
-    parser.add_argument("username", type=str)
+    parser.add_argument("username", type=str, required=True)
     parser.add_argument("name", type=str, required=True)
     parser.add_argument("password",type=str, required=True)
 #end class
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build
+docker-compose up --build
